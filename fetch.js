7ffc6557cea3efa7a8331420b238d6c983db0c8b
ferucci <development.first1@gmail.com>
2022-05-03 15:49:56 +0300

    changed bags

--- fetch.js
+++ fetch.js
@@ -8,9 +8,6 @@ const getData = (url) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      username.value = `My name is ${data.user}`
-      role.value = `I'm ${data.role}`
-      age.value = `My age is ${data.age}`
       sendData(data)
     })
     .catch((error) => console.log(error))
@@ -23,7 +20,15 @@ const sendData = (data) => {
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  }).catch((error) => console.log(error))
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      username.value = `My name is ${data.user}`
+      role.value = `I'm ${data.role}`
+      age.value = `My age is ${data.age}`
+      console.log(data)
+    })
+    .catch((error) => console.log(error))
 }
 
 document.addEventListener('DOMContentLoaded', getData('db.json'))
