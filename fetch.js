14e4452b0f45dae70e92d643ed216ac91731ec61
ferucci <development.first1@gmail.com>
2022-05-03 14:29:27 +0300

    first commit

---
+++ fetch.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const username = document.getElementById('text');
+const role = document.getElementById('role');
+const age = document.getElementById('num');
+
+const getData = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      username.value = `My name is ${data.user}`
+      role.value = `I'm ${data.role}`
+      age.value = `My age is ${data.age}`
+      sendData(data)
+    })
+    .catch((error) => console.log(error))
+}
+
+const sendData = (data) => {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).catch((error) => console.log(error))
+}
+
+document.addEventListener('DOMContentLoaded', getData('db.json'))
+
